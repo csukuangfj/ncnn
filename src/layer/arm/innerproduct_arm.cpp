7a8a3fc042f7caaa353b5d100a4a15485dd054c4
Fangjun Kuang <csukuangfj@gmail.com>
2022-12-05 17:56:39 +0800

    For conv-emformer transducer and LSTM transducer from icefall (#7)
    * Add upack tuple/list input
    
    * Fix for conv-emfomer
    
    * fix for LSTM
    
    * apply code-format changes
    
    * Fixes for arm
    
    Co-authored-by: csukuangfj <csukuangfj@users.noreply.github.com>

--- src/layer/arm/innerproduct_arm.cpp
+++ src/layer/arm/innerproduct_arm.cpp
@@ -147,8 +147,13 @@ int InnerProduct_arm::destroy_pipeline(const Option& opt)
     return 0;
 }
 
-int InnerProduct_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Option& opt) const
+int InnerProduct_arm::forward(const Mat& _bottom_blob, Mat& top_blob, const Option& opt) const
 {
+    Mat bottom_blob = _bottom_blob;
+    if (bottom_blob.dims == 3 && bottom_blob.h == 1)
+    {
+        bottom_blob = bottom_blob.reshape(bottom_blob.w, bottom_blob.c);
+    }
 #if NCNN_INT8
     if (opt.use_int8_inference && int8_scale_term)
     {
@@ -436,7 +441,14 @@ int InnerProduct_arm::forward(const Mat& bottom_blob, Mat& top_blob, const Optio
 #endif // __ARM_NEON
     size_t out_elemsize = elemsize / elempack * out_elempack;
 
-    top_blob.create(num_output / out_elempack, out_elemsize, out_elempack, opt.blob_allocator);
+    if (bottom_blob.dims == 2 && bottom_blob.h == 1)
+    {
+        top_blob.create(num_output / out_elempack, 1, out_elemsize, out_elempack, opt.blob_allocator);
+    }
+    else
+    {
+        top_blob.create(num_output / out_elempack, out_elemsize, out_elempack, opt.blob_allocator);
+    }
     if (top_blob.empty())
         return -100;
 
